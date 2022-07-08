5333a2bfcc129ae2489428dd3a5a612ebe8be8d3
Suraj7014 <108930234+Suraj7014@users.noreply.github.com>
2022-07-08 16:13:26 +0530

    Add files via upload

---
+++ DbOrmContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+using TFLWebApp.Models;
+
+namespace TFLWebApp.DAL
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DbOrmContext : DbContext
+    {
+        public DbSet<Product> Product10 { get; set; }
+
+        public DbOrmContext() : base("WebAppCon")
+        {
+
+        }
+    }
+}
